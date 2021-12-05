bebaade48dc31d126310b0c1758d18deefec0ddb
Mike <mikesingleton.24@gmail.com>
2021-12-05 09:44:09 -0330

    fixed failure to start

--- game/ui/main_menu/_scripts/ExitButton.cs
+++ game/ui/main_menu/_scripts/ExitButton.cs
@@ -2,13 +2,13 @@ using Godot;
 
 public class ExitButton : Button
 {
-    public override void _Ready()
-    {
-        Connect("pressed", this, nameof(QuitGame));
-    }
+	public override void _Ready()
+	{
+		Connect("pressed", this, nameof(QuitGame));
+	}
 
-    private void QuitGame()
-    {
-        GetTree().Quit(0);
-    }
+	private void QuitGame()
+	{
+		GetTree().Quit(0);
+	}
 }
