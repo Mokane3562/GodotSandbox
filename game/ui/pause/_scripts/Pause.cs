d20e08ea610b1292147edbc542219565a8847789
Mike <mikesingleton.24@gmail.com>
2021-12-12 23:02:08 -0330

    pause menu

--- game/ui/pause/_scripts/Pause.cs
+++ game/ui/pause/_scripts/Pause.cs
@@ -3,13 +3,29 @@ using System;
 
 public class Pause : Control
 {
+	
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("pause"))
 		{
-			var newPauseState = !GetTree().Paused;
-			GetTree().Paused = newPauseState;
-			Visible = newPauseState;
+			TogglePaused();
 		}
+	}
+	
+	private void TogglePaused()
+	{
+		var newPauseState = !GetTree().Paused;
+		GetTree().Paused = newPauseState;
+		Visible = newPauseState;
+	}
+	
+	private void _on_ResumeButton_pressed()
+	{
+		TogglePaused();
+	}
+	
+	private void _on_QuitButton_pressed()
+	{
+		GetTree().Quit(0);
 	}
 }
