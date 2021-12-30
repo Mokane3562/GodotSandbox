7795ebddac7c5db15fdb0ff7f7a9c1e579004a00
Mike <mikesingleton.24@gmail.com>
2021-12-30 19:33:49 +0300

    level selector

--- game/ui/main_menu/_scripts/MainMenu.cs
+++ game/ui/main_menu/_scripts/MainMenu.cs
@@ -3,10 +3,11 @@ using System;
 
 public class MainMenu : Control
 {
-
+	
 	private void _on_PlayGameButton_pressed()
 	{
-		GD.Print(new NotImplementedException(nameof(_on_PlayGameButton_pressed)));
+		GetNode<Control>("Sign").Visible = false;
+		GetNode<Control>("LevelSelector").Visible = true;
 	}
 
 	private void _on_LoadGameButton_pressed()
@@ -22,5 +23,21 @@ public class MainMenu : Control
 	private void _on_ExitButton_pressed()
 	{
 		GetTree().Quit(0);
+	}
+	
+	private void _on_BackButton_pressed()
+	{
+		GetNode<Control>("Sign").Visible = true;
+		GetNode<Control>("LevelSelector").Visible = false;
+	}
+	
+	private void _on_IsoTownButton_pressed()
+	{
+		GetTree().ChangeScene("res://levels/iso_town/IsoTown.tscn");
+	}
+
+	private void _on_TownButton_pressed()
+	{
+		GetTree().ChangeScene("res://levels/town/Town.tscn");
 	}
 }
